1b44ac57fa05c89332842665a06f8f9d3cd629ee
niloy.sarkar@learner.manipal.edu <niloy.sarkar@learner.manipal.edu>
2020-11-05 15:41:58 +0530

    Ab hoga

--- src/main/java/com/javatechie/crud/example/controller/ProductController.java
+++ src/main/java/com/javatechie/crud/example/controller/ProductController.java
@@ -4,47 +4,61 @@ import com.javatechie.crud.example.entity.Product;
 import com.javatechie.crud.example.repository.MyEntityRepository;
 import com.javatechie.crud.example.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class ProductController {
 
     @Autowired
     private ProductService service;
 
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+
+    @ResponseBody
     @PostMapping("/addProduct")
     public Product addProduct(@RequestBody Product product) {
         return service.saveProduct(product);
     }
 
+    @ResponseBody
     @PostMapping("/addProducts")
     public List<Product> addProducts(@RequestBody List<Product> products) {
         return service.saveProducts(products);
     }
 
+    @ResponseBody
     @GetMapping("/products")
     public List<Product> findAllProducts() {
         return service.getProducts();
     }
 
+    @ResponseBody
     @GetMapping("/productById/{id}")
     public Product findProductById(@PathVariable int id) {
         return service.getProductById(id);
     }
 
+    @ResponseBody
     @GetMapping("/product/{name}")
     public Product findProductByName(@PathVariable String name) {
         System.out.println(service.existsByName(name));
         return service.getProductByName(name);
     }
 
+    @ResponseBody
     @PutMapping("/update")
     public Product updateProduct(@RequestBody Product product) {
         return service.updateProduct(product);
     }
 
+    @ResponseBody
     @DeleteMapping("/delete/{id}")
     public String deleteProduct(@PathVariable int id) {
         return service.deleteProduct(id);
